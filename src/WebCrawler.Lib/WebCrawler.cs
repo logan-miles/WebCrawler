ed75bc8a7a8ce3a42b66bf77a5495333422e805b
logan <logan.e.miles@gmail.com>
2019-12-27 08:55:14 -0700

    Http request mocking works

--- src/WebCrawler.Lib/WebCrawler.cs
+++ src/WebCrawler.Lib/WebCrawler.cs
@@ -8,6 +8,9 @@ namespace WebCrawler.Lib
         private IHttpClientFactory factory;
         private Uri uri;
         public WebCrawler(IHttpClientFactory factory, string uri) {
+            if (!Uri.IsWellFormedUriString(uri, UriKind.Absolute))
+                throw new Exception("Invalid URL, Jerk!");
+            
             this.factory = factory;
             this.uri = new Uri(uri);
         }
--- tests/WebCrawler.Tests/WebCrawlerTests.cs
+++ tests/WebCrawler.Tests/WebCrawlerTests.cs
@@ -2,6 +2,7 @@ using Xunit;
 using Moq;
 using System.Net.Http;
 using Moq.Contrib.HttpClient;
+using FluentAssertions;
 
 namespace WebCrawler.Tests
 {
@@ -11,14 +12,27 @@ namespace WebCrawler.Tests
         public WebCrawlerTests() {
             var handler = new Mock<HttpMessageHandler>();
             factory = handler.CreateClientFactory();
+            SetupHandler(handler);
+            
 
 
+        }
+
+        [Theory]
+        [InlineData("https://www.logan.com", "<html><head><title>Logan Main</title></head><body><a href=\"https://www.google.com\"</body></html>", "Main")]
+        [InlineData("https://www.logan.com/about", "<html><head><title>Logan About</title></head><body><a href=\"https://www.google.com\"</body></html>", "About")]
+        public void TestMock(string uri, string expected, string message) {
+            var b = factory.CreateClient();
+            b.GetStringAsync(uri).Result
+                .Should().Be(expected, message);
         }
 
         private void SetupHandler(Mock<HttpMessageHandler> handler) {
             string baseUrl = "https://www.logan.com";
             handler.SetupRequest(HttpMethod.Get, $"{baseUrl}")
                 .ReturnsResponse("<html><head><title>Logan Main</title></head><body><a href=\"https://www.google.com\"</body></html>");
+            handler.SetupRequest(HttpMethod.Get, $"{baseUrl}/about")
+                .ReturnsResponse("<html><head><title>Logan About</title></head><body><a href=\"https://www.google.com\"</body></html>");    
         }
     }
 }
