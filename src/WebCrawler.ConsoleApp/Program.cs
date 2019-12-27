f8d8a5ee581447281bd68863433011bc48d35980
logan <logan.e.miles@gmail.com>
2019-12-27 13:25:54 -0700

    Starting console app for run.

---
+++ src/WebCrawler.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebCrawler.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
